94dbd78078aa0211afdeb5bba745eeafce7e3e09
nazifaRahimi <nazifarahimy6@gmail.com>
2025-03-05 20:48:20 -0800

    Fix repository name in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/text2/', // نام مخزن خود را اینجا بنویسید است
+  base: '/test2/', // نام مخزن خود را اینجا بنویسید است
   build: {
     outDir: 'docs' // خروجی build را در پوشه‌ی docs قرار بده
   }
